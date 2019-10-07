58e2cbbc8a1012277aa018e734106a483579f264
Pierre Schutz <pierreschutz@rocketmail.com>
2019-10-07 16:17:17 +0200

    Add basic structure

---
+++ components/ResumeItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card :color="color" dark>
+    <v-list-item three-line>
+      <v-list-item-content class="align-self-start">
+        <v-list-item-title class="headline mb-2" v-text="title"></v-list-item-title>
+        <v-list-item-subtitle :v-text="location + ' bonjoru  ' +  date"></v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item-avatar size="125" tile>
+      <v-img :src="img_src"></v-img>
+    </v-list-item-avatar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    color: String,
+    img_src: String,
+    title: String,
+    description: String,
+    date: String,
+    location: String
+  }
+};
+</script>
+
---
+++ layouts/default.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app dark>
+    <v-content>
+      <v-container>
+        <v-parallax
+          class="background-img"
+          src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+        >
+          <nuxt />
+        </v-parallax>
+      </v-container>
+    </v-content>
+
+    <v-footer :fixed="fixed" app>
+      <span>&copy; Pierre Schutz 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/"
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire"
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.background-img {
+  height: 100dpi;
+}
+</style>
